a456aec16b44ad3e2817a5a68af291c2b84ce587
Pancake <834430775@qq.com>
2020-12-11 00:43:06 +0800

    webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import x from './x.js'
-console.log(x)
+import png from '../images/1.png'
+
+const div = document.querySelector('#app')
+
+div.innerHTML = `
+    <img src="${png}">
+`
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick=()=>{
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    },()=>{
+        console.log('模块加载错误')
+    })
+}
+div.appendChild(button)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,5 +12,34 @@ module.exports = {
             title: 'My App',
             template: 'src/index.html',
         }),
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.styl$/,
+                loader: ["style-loader","css-loader","stylus-loader"] 
+            },
+            {
+                test: /\.less$/,
+                loader: ["style-loader","css-loader","less-loader"]
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                "style-loader",
+                "css-loader",
+                {
+                    loader: "sass-loader",
+                    options: {
+                        implementation: require('dart-sass')
+                    }
+                },
+                ],
+            },
+        ],
+      },
 };
